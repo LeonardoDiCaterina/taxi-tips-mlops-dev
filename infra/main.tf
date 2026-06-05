5d9b1f15c5f8de68d6fd8e63c40ad6a48f6b92e5
LeonardoDiCaterina <20240485@novaims.unl.pt>
2026-06-05 14:57:50 +0200

    Configure Terraform to use Google Cloud Storage for state management

--- infra/main.tf
+++ infra/main.tf
@@ -1,4 +1,3 @@
-# 1. Specify which provider (plugin) we need. We are using Google Cloud.
 terraform {
   required_providers {
     google = {
@@ -6,18 +5,10 @@ terraform {
       version = "~> 5.0"
     }
   }
-}
 
-# 2. Configure the Google Cloud provider with our specific project and region
-provider "google" {
-  project = "taxi-tips-mlops-dev"
-  region  = "europe-west1"
-}
-
-# 3. Declare our first resource: An Artifact Registry repository
-resource "google_artifact_registry_repository" "docker_repo" {
-  location      = "europe-west1"
-  repository_id = "mlops-docker-repo" # Name of the repo in GCP
-  description   = "Docker repository for MLOps project managed by Terraform"
-  format        = "DOCKER"
+  # THIS IS THE NEW PART: Move the state file to Cloud Storage
+  backend "gcs" {
+    bucket = "taxi-tips-mlops-tf-state-taxi-tips-mlops-dev"
+    prefix = "terraform/state"
+  }
 }
