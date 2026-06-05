b1876d96177069200e3808e9d4a332cc809bb228
LeonardoDiCaterina <20240485@novaims.unl.pt>
2026-06-05 15:16:24 +0200

    Add Terraform configuration for Google Cloud resources and variables

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,4 @@
+# 1. Specify which provider (plugin) we need
 terraform {
   required_providers {
     google = {
@@ -6,9 +7,61 @@ terraform {
     }
   }
 
-  # THIS IS THE NEW PART: Move the state file to Cloud Storage
   backend "gcs" {
-    bucket = "taxi-tips-mlops-tf-state-taxi-tips-mlops-dev"
+    # Note: State bucket names must be hardcoded in the backend block 
+    # because variables aren't loaded yet when Terraform initializes.
+    bucket = "taxi-tips-mlops-tf-state-taxi-tips-mlops-dev" 
     prefix = "terraform/state"
   }
+}
+
+# 2. Configure the Google Cloud provider using variables!
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+# 3. Declare our Artifact Registry repository
+resource "google_artifact_registry_repository" "docker_repo" {
+  location      = var.region
+  repository_id = "mlops-docker-repo"
+  description   = "Docker repository for MLOps project managed by Terraform"
+  format        = "DOCKER"
+}
+
+# 4. Deploy the FastAPI Cloud Run Service
+resource "google_cloud_run_v2_service" "api_service" {
+  name     = "${var.service_name}-${var.env}"
+  location = var.region
+  ingress  = "INGRESS_TRAFFIC_ALL"
+
+  template {
+    containers {
+      # Injecting the variables directly into the image string
+      image = "${var.region}-docker.pkg.dev/${var.project_id}/mlops-repo/hello:${var.image_tag}"
+      
+      resources {
+        limits = {
+          cpu    = var.cpu_limit
+          memory = var.memory_limit
+        }
+      }
+    }
+  }
+
+  depends_on = [google_artifact_registry_repository.docker_repo]
+}
+
+# 5. Make the Cloud Run service public
+resource "google_cloud_run_v2_service_iam_member" "public_access" {
+  project  = google_cloud_run_v2_service.api_service.project
+  location = google_cloud_run_v2_service.api_service.location
+  name     = google_cloud_run_v2_service.api_service.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
+
+# 6. Output the final URL so we can click it
+output "api_url" {
+  value = google_cloud_run_v2_service.api_service.uri
 }
---
+++ infra/variables.tf
@@ -0,0 +1,41 @@
+variable "project_id" {
+  description = "The GCP Project ID"
+  type        = string
+  default     = "taxi-tips-mlops-dev"
+}
+
+variable "region" {
+  description = "The GCP region to deploy resources in"
+  type        = string
+  default     = "europe-west1"
+}
+
+variable "env" {
+  description = "The environment to deploy resources in (e.g., dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "image_tag" {
+    description = "The Docker image tag to use for the model training and prediction jobs"
+    type        = string
+    default     = "v1"
+    }
+
+variable "service_name" {
+    description = "The name of the Cloud Run service to deploy the model to"
+    type        = string
+    default     = "taxi-tips-api"
+    }
+
+variable "memory_limit" {
+  description = "Memory limit for the Cloud Run container"
+  type        = string
+  default     = "512Mi"
+}
+
+variable "cpu_limit" {
+  description = "CPU limit for the Cloud Run container"
+  type        = string
+  default     = "1"
+}
