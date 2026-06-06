dfa602fe277007ac028ef7741231d5fbfa86ae1a
LeonardoDiCaterina <20240485@novaims.unl.pt>
2026-06-06 16:51:43 +0200

    Fix: Correct GitHub repository name casing in Terraform variables

--- infra/variables.tf
+++ infra/variables.tf
@@ -43,5 +43,5 @@ variable "cpu_limit" {
 variable "github_repo" {
   description = "The GitHub repository in the format owner/repo"
   type        = string
-  default     = "leonardodicaterina/taxi-tips-mlops-dev"
+  default     = "LeonardoDiCaterina/taxi-tips-mlops-dev"
 }
